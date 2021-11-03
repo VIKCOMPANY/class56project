f5b3cbb7ebbe972b974dcaff17ab90d68be6dc9a
VIKCOMPANY <89483719+VIKCOMPANY@users.noreply.github.com>
2021-11-02 20:00:06 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function clicked()
-{
-    document.getElementById("h1_tag").style.color="lightgreen";
+function clicked() {
+    document.getElementById("h1_tag").style.color = "lightgreen";
 }
 
+function imagef() {
+    document.getElementById("image").style.width="300px";
+    document.getElementById("image").style.height="300px";
+}
